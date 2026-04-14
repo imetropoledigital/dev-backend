3ceda5b1557f2aa2b866c04abce296726cdc0a96
Gustavo Leitão <gustavo.leitao@imd.ufrn.br>
2026-04-14 17:18:08 -0300

    adiciona conteúdo da aula04

--- index.js
+++ index.js
@@ -1,39 +1,65 @@
-const http = require('http')
+const express = require('express')
+const { v4: uuidv4 } = require('uuid');
+const app = express()
+app.use(express.json())
 
-const server = http.createServer((req, res) => {
-    console.log(req.url)
+const alunos = {}
+const PORT = 8080
 
-    switch (req.url) {
-        case '/aluno':
-            alunoRoute(req, res)
-            break;
-        default:
-            res.writeHead(404, { 'Content-Type': "text/json" })
-            res.write(JSON.stringify({ msg: "Path não encontrado" }))
-            res.end()
+app.get('/', (req, res) => {
+    res.json({msg: "Hello from Express!"})
+})
+
+app.get('/alunos/:id', (req, res) => {
+    res.json({aluno: alunos[req.params.id]})
+})
+
+app.put('/alunos', (req, res) => {
+    const id = req.query.id
+    if (id && alunos[id]){
+        const aluno = req.body
+        aluno.id = id
+        alunos[id] = aluno
+        res.json({msg: "Aluno atualizado com sucesso!"})
+    }else{
+        res.status(400).json({msg: "Aluno não encontrado!"})
+    }
+
+})
+
+app.delete('/alunos', (req, res) => {
+    const id = req.query.id
+    if (id && alunos[id]){
+        delete alunos[id]
+        res.json({msg: "Aluno deletado com sucesso!"})
+    }else{
+        res.status(400).json({msg: "Aluno não encontrado!"})
     }
 })
 
-server.listen(8080, () => {
-    console.log('Servidor pronto na porta 8080!')
+app.post('/alunos', (req, res) => {
+    const aluno = req.body
+    const idAluno = uuidv4()
+    aluno.id = idAluno
+    alunos[idAluno] = aluno
+    res.json({msg: "Aluno adicionado com sucesso!"})
 })
 
-function alunoRoute(req, res) {
-    switch (req.method){
-        case 'GET':
-            res.writeHead(200, { 'Content-Type': "text/json" })
-            res.write(JSON.stringify({ alunos: ["Gustavo", "João"]  }))
-            res.end()
-            break;
-        case 'POST':
-            res.writeHead(200, { 'Content-Type': "text/json" })
-            res.write(JSON.stringify({ msg: "Aluno criado" }))
-            res.end()
-            break
-        default:
-            res.writeHead(400, { 'Content-Type': "text/json" })
-            res.write(JSON.stringify({ msg: "Operação não suportada!" }))
-            res.end()
-            break
+app.get('/alunos', (req, res) => {
+    res.json({alunos: Object.values(alunos)})
+})
+
+const server = app.listen(PORT)
+
+server.on('listening', () => {
+    console.log(`Servidor pronto na porta ${PORT}`)
+})
+
+server.on('error', (err) => {
+    if (err.code === 'EADDRINUSE') {
+        console.error(`Erro: a porta ${PORT} já está em uso. Use outra porta ou encerre o processo que a está usando.`)
+    } else {
+        console.error('Erro ao iniciar o servidor:', err.message)
     }
-}
+    process.exit(1)
+})
