b95ab2b2209273be1ddf93e338c2b0b69d15e32c
Gustavo Leitão <gustavo.leitao@imd.ufrn.br>
2026-04-17 10:56:06 -0300

    nova aula 05

--- index.js
+++ index.js
@@ -1,52 +1,17 @@
-const express = require('express')
-const { v4: uuidv4 } = require('uuid');
+const express = require('express');
+const rotaUsuario = require('./rotas/usuario.rota')
+const rotaPost = require('./rotas/post.rota')
+
 const app = express()
 app.use(express.json())
 
-const alunos = {}
 const PORT = 8080
+
+app.use('/usuarios', rotaUsuario)
+app.use('/posts', rotaPost)
 
 app.get('/', (req, res) => {
     res.json({msg: "Hello from Express!"})
-})
-
-app.get('/alunos/:id', (req, res) => {
-    res.json({aluno: alunos[req.params.id]})
-})
-
-app.put('/alunos', (req, res) => {
-    const id = req.query.id
-    if (id && alunos[id]){
-        const aluno = req.body
-        aluno.id = id
-        alunos[id] = aluno
-        res.json({msg: "Aluno atualizado com sucesso!"})
-    }else{
-        res.status(400).json({msg: "Aluno não encontrado!"})
-    }
-
-})
-
-app.delete('/alunos', (req, res) => {
-    const id = req.query.id
-    if (id && alunos[id]){
-        delete alunos[id]
-        res.json({msg: "Aluno deletado com sucesso!"})
-    }else{
-        res.status(400).json({msg: "Aluno não encontrado!"})
-    }
-})
-
-app.post('/alunos', (req, res) => {
-    const aluno = req.body
-    const idAluno = uuidv4()
-    aluno.id = idAluno
-    alunos[idAluno] = aluno
-    res.json({msg: "Aluno adicionado com sucesso!"})
-})
-
-app.get('/alunos', (req, res) => {
-    res.json({alunos: Object.values(alunos)})
 })
 
 const server = app.listen(PORT)
